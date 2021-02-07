c0321d2ea43d7587951380a408a183264eb51d21
soozijan <75090137+soozijan@users.noreply.github.com>
2021-02-07 09:54:07 +0200

    Create Mealclass.java

---
+++ Mealclass.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+@AllArgsConstructor
+@ToString
+
+
+public class Meal {
+    public String name;
+    public boolean tivoni;
+    public float price;
+
+
+    //public Meal(String name, boolean tivoni, float price) {
+      //  this.name = name;
+      //  this.tivoni = tivoni;
+      //  this.price = price;
+  //  }
+}
